d311266404696aac3711b54eb8480e2fc7a8119f
Marvin <mjacobsz@gmail.com>
2018-12-07 19:00:30 +0100

    HD 16

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -3,22 +3,32 @@ defmodule Servy.BearController do
   alias Servy.Bear
   alias Servy.Wildthings
 
-  def index( %Conv{ method: "GET", path: "/bears" } = conv ) do
-    items =
-      Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
-      |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&Bear.as_html_list_item/1)
+  @templates_path Path.expand("../../templates", __DIR__)
 
-    %{ conv | status: 200, resp_body: "<ul>#{items}</ul>" }
+  defp render(conv, template, bindings \\ []) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %{ conv | status: 200, resp_body: content }
+  end
+
+  def index( %Conv{ method: "GET", path: "/bears" } = conv ) do
+    bears =
+      Wildthings.list_bears()
+      |> Enum.sort(&Bear.order_asc_by_name/2)
+
+    render(conv, "index.eex", bears: bears)
   end
 
   def show( %Conv{ method: "GET" } = conv, %{"id" => id} ) do
     bear = Wildthings.get_bear(id)
-    %{ conv | status: 200, resp_body: "<h1>Here's your bear, sir: #{id}. #{bear.name} </h1>" }
+
+    render(conv, "show.eex", bear: bear)
   end
 
-  def create( conv, %{ "name" => name, "type" => type } = params ) do
+  def create( conv, %{ "name" => name, "type" => type }) do
     IO.puts "Create a bear with #{name} and #{type}"
     %{ conv | status: 201, resp_body: "I created your fucking bear asshole" }
   end
